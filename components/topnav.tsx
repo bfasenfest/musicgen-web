52a11e9a8327387cc24cc4dc44f810d8c529df0e
bfasenfest <aowbackup@gmail.com>
2023-11-24 19:37:32 -0500

    sign in toggle

--- components/topnav.tsx
+++ components/topnav.tsx
@@ -1,24 +1,33 @@
 "use client";
 
 import { Button } from "./ui/button";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
 import MobileSidebar from "./mobile-sidebar";
+import Link from "next/link";
 
 const TopNav = () => {
+  const user = useUser();
+
   const supabase = useSupabaseClient();
   const router = useRouter();
 
   async function signOut() {
-    const { error } = await supabase.auth.signOut();
     router.push("/");
+    const { error } = await supabase.auth.signOut();
   }
 
   return (
     <div className="flex items-center p-4">
       <MobileSidebar />
       <div className="flex w-full justify-end">
-        <Button onClick={() => signOut()}>Sign Out</Button>
+        {user ? (
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        ) : (
+          <Link href="/sign-in">
+            <Button>Login</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
