65ffa38cb7d18b08c790692a38615cd1293ce0a0
bfasenfest <aowbackup@gmail.com>
2023-11-25 22:05:12 -0500

    add in replicate model on melodygen

---
+++ app/(dashboard)/(routes)/melodygen/page.tsx
@@ -0,0 +1,404 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { ArrowDownToLine, Trash, Play } from "lucide-react";
+
+import { useVisualizer, models } from "react-audio-viz";
+
+import axios from "axios";
+
+import { useEffect, useState, useRef } from "react";
+
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+
+import { v4 as uuidv4 } from "uuid";
+
+import { decode } from "base64-arraybuffer";
+
+import { useTrackStore } from "@/lib/store";
+
+import Replicate from "replicate";
+
+type Track = {
+  prompt: string;
+  audio: string;
+};
+
+const CDNURL =
+  "https://qdciohgpchihhkgxlygz.supabase.co/storage/v1/object/public/tracks/";
+
+const API_URL = "https://3140-185-158-179-210.ngrok.io";
+
+const MelodyGenPage = () => {
+  const { tracks, setTracks } = useTrackStore();
+  const [prompt, updatePrompt] = useState();
+  const [queue, setQueue] = useState<string[]>([]);
+
+  const [currentTrack, setCurrentTrack] = useState();
+
+  const [trackLength, updateTrackLength] = useState<number>();
+  const [loading, updateLoading] = useState(false);
+  const [trackPlaying, updateTrackPlaying] = useState(false);
+
+  const audioRef = useRef(null);
+  const [AudioViz, init] = useVisualizer(audioRef);
+
+  const user = useUser();
+  const supabase = useSupabaseClient();
+
+  const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN,
+  });
+
+  const getTracks = async () => {
+    if (!tracks) updateLoading(true);
+
+    try {
+      const { data: tracks } = await supabase.storage
+        .from("tracks")
+        .list(user?.id + "/melody/", {
+          limit: 30,
+          offset: 0,
+          sortBy: {
+            column: "created_at",
+            order: "desc",
+          },
+        });
+
+      if (tracks !== null) {
+        setTracks(tracks);
+      }
+    } catch (e) {
+      alert(e);
+    } finally {
+      updateLoading(false);
+    }
+  };
+  useEffect(() => {
+    async function getLoader() {
+      const { quantum } = await import("ldrs");
+      quantum.register();
+    }
+    getLoader();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      getTracks();
+    }
+  }, [user]);
+
+  const generateTrack = async (prompt: string) => {
+    updateLoading(true);
+    setQueue((oldQueue) => [...oldQueue, prompt]);
+    length = Math.round(trackLength) || 5;
+    const track = await replicate.run(
+      "meta/musicgen:7be0f12c54a8d033a0fbd14418c9af98962da9a86f5ff7811f9b3423a1f0b7d7",
+      {
+        input: {
+          top_k: 250,
+          top_p: 0,
+          prompt,
+          duration: length,
+          temperature: 1,
+          continuation: false,
+          model_version: "stereo-large",
+          output_format: "wav",
+          continuation_start: 0,
+          multi_band_diffusion: false,
+          normalization_strategy: "peak",
+          classifier_free_guidance: 3,
+        },
+      }
+    );
+    console.log(track);
+
+    const { data, error } = await supabase.storage
+      .from("tracks")
+      .upload(
+        user?.id + "/melody/" + `${prompt}-${uuidv4()}.wav`,
+        decode(track.data),
+        {
+          contentType: "audio/wav",
+        }
+      );
+    setQueue((oldQueue) => oldQueue.filter((item) => item !== prompt));
+
+    if (error) {
+      alert(error);
+    } else {
+      getTracks();
+    }
+    if (queue.length === 0) updateLoading(false);
+  };
+
+  const deleteTrack = async (trackName: String) => {
+    const { error } = await supabase.storage
+      .from("tracks")
+      .remove([user?.id + "/" + trackName]);
+
+    if (error) {
+      alert(error);
+    } else {
+      getTracks();
+    }
+  };
+
+  const initTrack = () => {
+    init();
+    updateTrackPlaying(true);
+
+    if (audioRef.current) {
+      audioRef.current.onended = () => {
+        updateTrackPlaying(false);
+      };
+      audioRef.current.onpause = () => {
+        updateTrackPlaying(false);
+      };
+    }
+  };
+
+  const playTrack = (trackSrc: string) => {
+    setCurrentTrack(trackSrc);
+    init();
+    updateTrackPlaying(true);
+
+    if (audioRef.current) {
+      audioRef.current.oncanplaythrough = () => {
+        audioRef.current.play().catch((error) => console.log(error));
+        updateTrackPlaying(true);
+      };
+      audioRef.current.onended = () => {
+        updateTrackPlaying(false);
+      };
+      audioRef.current.onpause = () => {
+        updateTrackPlaying(false);
+      };
+      audioRef.current.load();
+    }
+  };
+
+  const saveTrack = (track) => {
+    const url = CDNURL + user.id + "/" + track.name;
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `${track.name}.wav`;
+    link.click();
+  };
+
+  return (
+    <div className="dark">
+      <div className="flex items-center justify-center">
+        <Card className=" w-5/6 md:w-3/6 ">
+          <CardHeader>
+            <div className="flex justify-between">
+              <div>
+                <CardTitle>Generate New Track</CardTitle>
+                <CardDescription>
+                  Generate track using the latest music-gen models
+                </CardDescription>
+              </div>
+
+              {loading ? (
+                <div className="">
+                  <l-quantum size="45" speed="1.75" color="white"></l-quantum>
+                </div>
+              ) : null}
+            </div>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Prompt</Label>
+                  <Input
+                    id="name"
+                    value={prompt}
+                    onChange={(e) => updatePrompt(e.target.value)}
+                    placeholder="Description of your music track. For Example: Classic Rock, Drum Kit, Electric Guitar, Bass, Raw, Uplifting, Anthem"
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Track Length (in seconds)</Label>
+                  <Input
+                    id="name"
+                    value={trackLength}
+                    onChange={(e) => updateTrackLength(e.target.value)}
+                    placeholder="Track length in seconds. For Example: 5"
+                  />
+                </div>
+                {/* <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="framework">Framework</Label>
+                  <Select>
+                    <SelectTrigger id="framework">
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="next">Next.js</SelectItem>
+                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                      <SelectItem value="astro">Astro</SelectItem>
+                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div> */}
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            {loading
+              ? `Working on ${queue[0]} | tracks left ${queue.length}`
+              : null}
+            <Button onClick={(e) => generateTrack(prompt || "")}>
+              Generate
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="hidden md:block w-5/12 ml-5">
+          <div className=" h-60 relative ">
+            {!trackPlaying ? (
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2top">
+                <l-quantum size="75" speed="3" color="white" />
+              </div>
+            ) : null}
+            <AudioViz
+              className="absolute inset-0 "
+              model={models.horizontal({
+                darkMode: true,
+                reversed: false,
+                fadeBars: true,
+                scale: 0.9,
+                color: "#F44E3B",
+                binSize: 25,
+                frequencyRange: [0, 16000],
+              })}
+            />
+          </div>
+          <div className="flex justify-center">
+            <audio
+              className="w-5/6 h-10 mb-5 mt-4"
+              controls={true}
+              ref={audioRef}
+              onPlay={initTrack}
+              crossOrigin="anonymous"
+              src={currentTrack}
+            />
+          </div>
+        </Card>
+      </div>
+
+      <div className="flex flex-wrap items-center justify-center mt-4 drop-shadow-md">
+        {loading ? (
+          <div className="flex items-center justify-center drop-shadow-md">
+            <Skeleton className="h-[150px] w-[300px] m-2 " />{" "}
+          </div>
+        ) : null}
+        {tracks.map((track) => (
+          <Card
+            className="h-[150px] w-[300px] m-2 relative"
+            key={CDNURL + user.id + "/" + track.name}
+          >
+            <CardHeader className="flex justify-between">
+              <CardTitle className="text-md">
+                {track.name.split("-")[0]}
+              </CardTitle>
+              <div>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button className="  m-4" size="icon" variant="destructive">
+                      <Trash />
+                    </Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent classnName="relative">
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Are you absolutely sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently
+                        delete this track from your account.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction
+                        onClick={(e) => deleteTrack(track.name)}
+                      >
+                        Continue
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+
+                <Button
+                  className=" m-4"
+                  onClick={(e) =>
+                    playTrack(CDNURL + user.id + "/" + track.name)
+                  }
+                  size="icon"
+                  variant="outline"
+                >
+                  <Play />
+                </Button>
+                <Button
+                  className=" m-4"
+                  onClick={(e) => saveTrack(track)}
+                  size="icon"
+                  variant="outline"
+                >
+                  <ArrowDownToLine />
+                </Button>
+              </div>
+            </CardHeader>
+            {/* <div className="flex justify-center">
+              <audio
+                className="w-5/6 h-10 mb-5 mt-4"
+                controls
+                key={CDNURL + user.id + "/" + track.name}
+              >
+                <source src={CDNURL + user.id + "/" + track.name} />
+                Your browser does not support the audio element.
+              </audio>
+            </div> */}
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MelodyGenPage;
