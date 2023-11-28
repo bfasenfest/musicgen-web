9309774c758afc1102e0cf2be8c83ee84cdd9556
bfasenfest <aowbackup@gmail.com>
2023-11-28 18:23:36 -0500

    Finish Pro Modal with loading and checkout api

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -10,28 +10,50 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import { motion } from "framer-motion";
+
 import { Button } from "@/components/ui/button";
 import { Zap } from "lucide-react";
 
 import { useProModal } from "@/lib/pro-modal";
 
 import { useState } from "react";
+import { useEffect } from "react";
 
 const ProModal = () => {
   const proModal = useProModal();
 
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    async function getLoader() {
+      const { grid } = await import("ldrs");
+      grid.register();
+    }
+    getLoader();
+  }, []);
+
   const onSubscribe = async () => {
     try {
       setLoading(true);
-      //   const response = await axios.get("/api/stripe");
 
-      //   window.location.href = response.data.url;
+      const response: any = await fetch("/api/checkout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      });
+
+      let session = await response.json();
+
+      console.log(session);
+
+      window.location.href = session.url;
     } catch (error) {
-      alert("Something went wrong");
+      alert(error);
     } finally {
-      setLoading(false);
+      // setLoading(false);
     }
   };
 
@@ -39,24 +61,45 @@ const ProModal = () => {
     <div>
       <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
         <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Upgrade account to Pro</DialogTitle>
-            <DialogDescription>
-              Free accounts are limited to 25 generations per month. Upgrade to
-              a Pro account for unlimited generations.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Button
-              disabled={loading}
-              onClick={onSubscribe}
-              size="lg"
-              className="w-full transition duration-400  ease-in-out bg-gradient-to-r from-cyan-500 to-cyan-700 hover:from-cyan-600 hover:to-cyan-800 hover:scale-105"
+          {loading ? (
+            <motion.div
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ ease: "easeIn", duration: 0.3 }}
             >
-              Upgrade
-              <Zap className="w-4 h-4 ml-2 fill-white" />
-            </Button>
-          </DialogFooter>
+              <DialogHeader>
+                <div className="flex flex-col justify-center items-center m-2">
+                  <h1 className="text-color-cyan-500 text-xl font-bold m-2 mb-6">
+                    Loading Stripe Session
+                  </h1>
+                  <l-grid size="80" speed="1.5" color="black"></l-grid>
+                </div>
+              </DialogHeader>
+            </motion.div>
+          ) : (
+            <div>
+              <DialogHeader>
+                <DialogTitle>Upgrade account to Pro</DialogTitle>
+                <DialogDescription>
+                  Free accounts are limited to 25 generations per month. Upgrade
+                  to a Pro account for unlimited generations.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <div className="flex flex-col w-full mt-5">
+                  <Button
+                    disabled={loading}
+                    onClick={onSubscribe}
+                    size="lg"
+                    className="w-full transition duration-400  ease-in-out bg-gradient-to-r from-cyan-500 to-cyan-700 hover:from-cyan-600 hover:to-cyan-800 hover:scale-105"
+                  >
+                    Upgrade
+                    <Zap className="w-4 h-4 ml-2 fill-white" />
+                  </Button>
+                </div>
+              </DialogFooter>
+            </div>
+          )}
         </DialogContent>
       </Dialog>
     </div>
